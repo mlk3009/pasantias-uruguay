feeecac7b3f8f0713c68c8f41591a33fd10c1211
Lucas Herrera <lucasherrera3009@gmail.com>
2024-04-30 10:34:28 -0300

    componente para cargar publicaciones

---
+++ Frontend/src/app/components/create-publication/create-publication.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-publication',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-publication.component.html',
+  styleUrl: './create-publication.component.css'
+})
+export class CreatePublicationComponent {
+
+}
--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -1,30 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { User } from "../models/user";
-import { global } from "./global";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { global } from './global';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class UserService {
-
-  constructor(public _http: HttpClient) { }
+  constructor(public _http: HttpClient) {}
 
   register(user: User): Observable<any> {
     let json = JSON.stringify(user);
     let params = json;
 
     let headers = new HttpHeaders().set('Content-Type', 'application/raw');
-    return this._http.post(global.url + 'register', params, { headers: headers });
+    return this._http.post(global.url + 'register', params, {
+      headers: headers,
+    });
   }
 
   login(user: User): Observable<any> {
-
     var loginInformation = {
-      'email': user.email,
-      'password': user.password
+      email: user.email,
+      password: user.password,
     };
 
     let params = JSON.stringify(loginInformation);
@@ -33,7 +32,6 @@ export class UserService {
   }
 
   obtenerUsuario(token: string): Observable<any> {
-
     const headers = new HttpHeaders({
       Authorization: `Bearer ${token}`,
     });
@@ -43,36 +41,41 @@ export class UserService {
 
   restore(user: User): Observable<any> {
     var json = {
-      'email': user.email
+      email: user.email,
     };
     let params = JSON.stringify(json);
 
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return this._http.post(global.url + 'restore', params, { headers: headers });
+    return this._http.post(global.url + 'restore', params, {
+      headers: headers,
+    });
   }
 
   checkCode(email: any, code: any): Observable<any> {
     var json = {
-      'email': email,
-      'code': code
+      email: email,
+      code: code,
     };
 
     let params = JSON.stringify(json);
 
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return this._http.post(global.url + 'checkCode', params, { headers: headers });
+    return this._http.post(global.url + 'checkCode', params, {
+      headers: headers,
+    });
   }
 
   changePassword(email: any, password: any): Observable<any> {
     var json = {
-      'email': email,
-      'password': password
+      email: email,
+      password: password,
     };
 
     let params = JSON.stringify(json);
 
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return this._http.put(global.url + 'changePassword', params, { headers: headers });
+    return this._http.put(global.url + 'changePassword', params, {
+      headers: headers,
+    });
   }
-
 }
