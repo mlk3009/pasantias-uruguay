6be7d933929aa2ab84c98eb096f07cae1cedd2bb
Alex Perez <adamianperez224@gmail.com>
2024-06-11 00:34:44 -0300

    backend al docker

---
+++ docker-laravel-angular-master/backend/routes/api.php
@@ -0,0 +1,46 @@
+<?php
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\PasswordController;
+use App\Http\Controllers\Api\VerificationController;
+use App\Http\Controllers\Api\PublicationController;
+
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// USER 
+Route::post('login', [UserController::class, 'loginUser']);
+Route::post('register', [UserController::class, 'store']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('user', [UserController::class, 'userDetails']);
+    Route::post('logout', [UserController::class, 'logout']);
+});
+
+
+// PASSWORD
+Route::post('restore', [PasswordController::class, 'restorePassword']);
+Route::post('checkCode', [PasswordController::class, 'checkCode']);
+Route::put('changePassword', [UserController::class, 'changePassword']);
+
+
+
+// EMAIL VERIFICATION
+// Ruta para verificar el correo electrónico
+Route::post('checkEmailCode', [UserController::class, 'checkEmailCode']);
+
+
+Route::get('/publications', [PublicationController::class, 'index']);
+Route::get('/publications/show/{id}', [PublicationController::class, 'show']);
+Route::post('/publications/store', [PublicationController::class, 'store']);
+Route::put('/publications/update/{id}', [PublicationController::class, 'update']);
+Route::patch('/publications/updatePartial/{id}', [PublicationController::class, 'updatePartial']);
+Route::delete('/publications/destroy/{id}', [PublicationController::class, 'destroy']);
+
+
