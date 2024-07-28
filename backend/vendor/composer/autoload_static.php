925b0e2d83bd44973373dc3f60f48b8a25093f4d
Dess24 <adamianperez224@gmail.com>
2024-07-28 02:26:59 -0300

    a

--- backend/vendor/composer/autoload_static.php
+++ backend/vendor/composer/autoload_static.php
@@ -369,8 +369,8 @@ class ComposerStaticInit2ca0f5ad6672a20fa7a764ac586aa25f
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '78922d93f235bd00a54d35a6cc7fb6e3fc31310a',
+        'reference' => '38b5b5a4205e6951e91aa4611d6f72175bd527ba',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -391,7 +391,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '78922d93f235bd00a54d35a6cc7fb6e3fc31310a',
+            'reference' => '38b5b5a4205e6951e91aa4611d6f72175bd527ba',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
