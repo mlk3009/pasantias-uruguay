e79617f618b524e73f3b374656e8fc3737d3e60b
Emanuel Quiroga <monode3cabezas21@gmail.com>
2024-11-28 18:57:23 -0300

    nada

--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '9041a160cb772cab5185e51f3ed265972f4733ab',
+        'reference' => 'd87b92987fcdc881fdef39bf6c1a8bf744f2c45f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -391,7 +391,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '9041a160cb772cab5185e51f3ed265972f4733ab',
+            'reference' => 'd87b92987fcdc881fdef39bf6c1a8bf744f2c45f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
